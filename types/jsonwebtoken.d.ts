b1e254a14bf66b5520a4dfcbaa6bee2d67cc0b74
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-06-14 17:48:17 +0530

    waitlist added

---
+++ types/jsonwebtoken.d.ts
@@ -0,0 +1,37 @@
+declare module 'jsonwebtoken' {
+  export function sign(payload: string | Buffer | object, secretOrPrivateKey: string | Buffer, options?: SignOptions): string;
+  export function verify(token: string, secretOrPublicKey: string | Buffer, options?: VerifyOptions & { complete?: boolean }): object | string;
+  export function decode(token: string, options?: DecodeOptions & { complete?: boolean }): null | { [key: string]: any } | string;
+
+  export interface SignOptions {
+    algorithm?: string;
+    expiresIn?: string | number;
+    notBefore?: string | number;
+    audience?: string | string[];
+    issuer?: string;
+    jwtid?: string;
+    subject?: string;
+    noTimestamp?: boolean;
+    header?: object;
+    encoding?: string;
+    keyid?: string;
+  }
+
+  export interface VerifyOptions {
+    algorithms?: string[];
+    audience?: string | string[];
+    clockTimestamp?: number;
+    clockTolerance?: number;
+    issuer?: string | string[];
+    ignoreExpiration?: boolean;
+    ignoreNotBefore?: boolean;
+    jwtid?: string;
+    subject?: string;
+    maxAge?: string | number;
+  }
+
+  export interface DecodeOptions {
+    complete?: boolean;
+    json?: boolean;
+  }
+} 
