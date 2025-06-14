f82590b7b2d06eb7de5b70c76d1303fc3ef51176
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-06-14 21:50:49 +0530

    small issue fix

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -93,6 +93,18 @@ const TableCell = React.forwardRef<
 ))
 TableCell.displayName = "TableCell"
 
+const TableCaption = React.forwardRef<
+  HTMLTableCaptionElement,
+  React.HTMLAttributes<HTMLTableCaptionElement>
+>(({ className, ...props }, ref) => (
+  <caption
+    ref={ref}
+    className={cn("mt-4 text-sm text-muted-foreground", className)}
+    {...props}
+  />
+))
+TableCaption.displayName = "TableCaption"
+
 export {
   Table,
   TableHeader,
@@ -101,4 +113,5 @@ export {
   TableHead,
   TableRow,
   TableCell,
+  TableCaption,
 }
