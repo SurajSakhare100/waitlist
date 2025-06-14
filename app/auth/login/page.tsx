180d0eb3aa5b9290268869b3511d9041dbad42a1
SurajSakhare100 <sakharesuraj10@gmail.com>
2025-06-14 21:32:30 +0530

    small issue fix

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,11 +3,11 @@
 import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import Link from "next/link"
 import { useRouter } from 'next/navigation'
 import { signIn } from 'next-auth/react'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
 
 export default function LoginPage() {
   const [email, setEmail] = useState('')
